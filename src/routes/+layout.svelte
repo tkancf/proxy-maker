24d69b4cc5a1344b3916fb88348312fb75ef04ee
tkancf <18424095+tkancf@users.noreply.github.com>
2023-10-12 23:48:31 +0900

    ページ遷移するように変更

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { setContext } from 'svelte';
+	let imageUrls: string[] = [];
+	setContext('imageUrls', imageUrls);
+</script>
+
+<slot />
---
+++ src/routes/show/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { getContext } from 'svelte';
+
+	const imageUrls = getContext('imageUrls');
+</script>
+
+<div class="grid">
+	{#each imageUrls as imageUrl, index (index)}
+		{#if imageUrl}
+			<img src={imageUrl} alt="User uploaded" />
+		{/if}
+	{/each}
+</div>
+
+<style>
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		gap: 10px;
+	}
+
+	img {
+		width: 100%;
+		height: auto;
+		border: 1px solid black;
+		box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+	}
+</style>
