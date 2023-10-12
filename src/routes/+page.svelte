8767800aeeb1b0e5b209a5d428befe86f1d769ef
tkancf <18424095+tkancf@users.noreply.github.com>
2023-10-12 09:26:09 +0900

    Changed image URL page into another page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let imageUrlInput = '';
+	let imageUrls: string[] = [];
+
+	function showImages() {
+		imageUrls = [...new Set(imageUrlInput.trim().split('\n'))];
+	}
+</script>
+
+<div>
+	<textarea bind:value={imageUrlInput} rows="9" placeholder="画像のURLを入力してください..." />
+	<button on:click={showImages}>画像を表示</button>
+
+	{#if imageUrls.length}
+		<div class="grid">
+			{#each imageUrls as imageUrl (imageUrl)}
+				{#if imageUrl}
+					<img src={imageUrl} alt="User uploaded" />
+				{/if}
+			{/each}
+		</div>
+	{/if}
+</div>
+
+<style>
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		gap: 10px;
+	}
+
+	img {
+		width: 100%;
+		height: auto;
+		border: 1px solid black;
+		box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+	}
+</style>
