100abc56ce8da923079a879936302dee37a48d47
tkancf <18424095+tkancf@users.noreply.github.com>
2023-10-12 23:48:31 +0900

    Change to page transition

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,17 @@
-<script lang="ts">
-	let imageUrlInput = '';
-	let imageUrls: string[] = [];
+<script>
+	import { getContext } from 'svelte';
+	import { goto } from '$app/navigation';
 
-	function showImages() {
-		imageUrls = [...imageUrlInput.trim().split('\n')];
+	const imageUrls = getContext('imageUrls');
+	let imageUrlInput = '';
+
+	function goToDisplay() {
+		imageUrls.push(...imageUrlInput.trim().split('\n'));
+		goto('/show');
 	}
 </script>
 
 <div>
 	<textarea bind:value={imageUrlInput} rows="9" placeholder="画像のURLを入力してください..." />
-	<button on:click={showImages}>画像を表示</button>
-
-	{#if imageUrls.length}
-		<div class="grid">
-			{#each imageUrls as imageUrl, index (index)}
-				{#if imageUrl}
-					<img src={imageUrl} alt="User uploaded" />
-				{/if}
-			{/each}
-		</div>
-	{/if}
+	<button on:click={goToDisplay}>画像を表示</button>
 </div>
-
-<style>
-	.grid {
-		display: grid;
-		grid-template-columns: repeat(3, 1fr);
-		gap: 10px;
-	}
-
-	img {
-		width: 100%;
-		height: auto;
-		border: 1px solid black;
-		box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
-	}
-</style>
