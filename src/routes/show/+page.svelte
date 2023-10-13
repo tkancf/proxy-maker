1a1a44815389fc2c7cab185ded4eee7b53029436
tkancf <18424095+tkancf@users.noreply.github.com>
2023-10-13 16:09:48 +0900

    Remove header when print

--- src/routes/show/+page.svelte
+++ src/routes/show/+page.svelte
@@ -12,21 +12,28 @@
 	{/each}
 </div>
 
+<svelte:head>
+	<style>
+		body,
+		html {
+			margin: 0;
+			padding: 0;
+		}
+	</style>
+</svelte:head>
+
 <style>
 	.grid {
 		display: grid;
 		grid-template-columns: repeat(3, 63mm);
-		width: 189mm; /* 3画像 x 63mm = 189mm */
-		height: 264mm; /* 3画像 x 88mm = 264mm */
-		margin: 0; /* 余白を削除 */
-		padding: 0; /* 余白を削除 */
-		box-sizing: border-box;
+		width: 189mm;
+		height: 264mm;
 	}
 
 	img {
 		width: 63mm;
 		height: 88mm;
-		object-fit: cover; /* 画像のアスペクト比を保持しながら、指定されたサイズに収める */
+		object-fit: cover;
 		border: 1px solid black;
 		box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
 	}
