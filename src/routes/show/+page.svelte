b9a72731c703ee9ff14ab64cc880f544118ccef6
tkancf <18424095+tkancf@users.noreply.github.com>
2023-10-13 16:02:38 +0900

    A4で印刷したときにいい感じになるように修正

--- src/routes/show/+page.svelte
+++ src/routes/show/+page.svelte
@@ -15,13 +15,18 @@
 <style>
 	.grid {
 		display: grid;
-		grid-template-columns: repeat(3, 1fr);
-		gap: 10px;
+		grid-template-columns: repeat(3, 63mm);
+		width: 189mm; /* 3画像 x 63mm = 189mm */
+		height: 264mm; /* 3画像 x 88mm = 264mm */
+		margin: 0; /* 余白を削除 */
+		padding: 0; /* 余白を削除 */
+		box-sizing: border-box;
 	}
 
 	img {
-		width: 100%;
-		height: auto;
+		width: 63mm;
+		height: 88mm;
+		object-fit: cover; /* 画像のアスペクト比を保持しながら、指定されたサイズに収める */
 		border: 1px solid black;
 		box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
 	}
