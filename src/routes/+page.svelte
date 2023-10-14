89bf2d6919fc74c347f4fd9e64b39ea0e6feea7b
tkancf <18424095+tkancf@users.noreply.github.com>
2023-10-15 01:12:59 +0900

    TailwindCSS をトップページへ適用

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,44 +9,20 @@
 		imageUrls.push(...imageUrlInput.trim().split('\n'));
 		goto('/show');
 	}
+	import '../app.css';
 </script>
 
-<div>
-	<textarea bind:value={imageUrlInput} rows="9" placeholder="画像のURLを入力してください..." />
-	<button on:click={goToDisplay}>画像を表示</button>
+<div class="bg-white p-10 rounded shadow-md">
+	<textarea
+		bind:value={imageUrlInput}
+		rows="9"
+		placeholder="画像のURLを入力してください..."
+		class="w-full p-2 text-lg border rounded mb-5 resize-none"
+	/>
+	<button
+		on:click={goToDisplay}
+		class="cursor-pointer bg-blue-500 text-white border-0 py-2 px-5 rounded text-lg transition-colors hover:bg-blue-700"
+	>
+		画像を表示
+	</button>
 </div>
-
-<style>
-	div {
-		background: white;
-		padding: 20px 40px;
-		border-radius: 15px;
-		box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-	}
-
-	textarea {
-		width: 100%;
-		padding: 10px;
-		font-size: 16px;
-		border: 1px solid #ddd;
-		border-radius: 5px;
-		box-sizing: border-box;
-		margin-bottom: 20px;
-		resize: none;
-	}
-
-	button {
-		cursor: pointer;
-		background-color: #f67280;
-		color: white;
-		border: none;
-		padding: 10px 20px;
-		border-radius: 5px;
-		font-size: 16px;
-		transition: background-color 0.3s ease;
-	}
-
-	button:hover {
-		background-color: #c06c84;
-	}
-</style>
