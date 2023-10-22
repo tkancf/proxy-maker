43c4e7233722989ae530256452a7c3ee47db8b4d
tkancf <18424095+tkancf@users.noreply.github.com>
2023-10-22 19:01:07 +0900

    リファクタ

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import { goto } from '$app/navigation';
 	let imageUrls: string[] = [];
 	setContext('imageUrls', imageUrls);
-	function backToTop() {
+	function gotoTop() {
 		goto('/');
 	}
 </script>
@@ -11,7 +11,7 @@
 <div class="bg-blue-500 p-4 text-white flex justify-between items-center">
 	<button
 		class="text-lg font-semibold bg-transparent border-none p-0 m-0 cursor-pointer focus:outline-none"
-		on:click={backToTop}
+		on:click={gotoTop}
 	>
 		Your App Name
 	</button>
